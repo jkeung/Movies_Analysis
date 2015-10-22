2d3c8eb31f87929eacd9b9a7a821f404817d230f
Jason Keung <jason.s.keung@gmail.com>
2015-10-22 13:41:21 -0400

    adding scrape_movie.py

--- scrape_movie.py
+++ scrape_movie.py
@@ -2,32 +2,23 @@ import os
 import luigi
 import util
 
-OUTPUTDIR = 'output'
-FILENAMETEMPLATE = 'movie_dictionary'
-
 class GetDataForLetter(luigi.Task):
 	letter = luigi.Parameter()
-	
-	def requires(self):
-		""" Upstream Tasks required for completion """
-		return None
 
 	def run(self):
-		""" Runs a job """
 		url_list = util.get_movie_url_list(self.letter)
-		output_path = os.path.join(OUTPUTDIR, '{0}_{1}.p'.format(FILENAMETEMPLATE, self.letter))
+		output_path = os.path.join(util.OUTPUTDIR, '{0}_{1}.p'.format(util.FILENAMETEMPLATE, self.letter))
 		util.update_movie_dictionary(output_path, url_list)
 
 	def output(self):
-		""" Checks for output to see if the task is complete """
-		return luigi.LocalTarget(os.path.join(OUTPUTDIR, '{0}_{1}.p'.format(FILENAMETEMPLATE, self.letter)))
+		return luigi.LocalTarget(os.path.join(util.OUTPUTDIR, '{0}_{1}.p'.format(util.FILENAMETEMPLATE, self.letter)))
+
 
 class RunManyLetters(luigi.Task):
-
+	
 	def requires(self):
-		""" Upstream Tasks required for completion """
 		for myletter in '#ABCDEFGHIJKLMNOPQRSTUVWXYZ':
-			yield GetDataForLetter(letter = myletter)
+			yield GetDataForLetter(letter=myletter)
 
 if __name__ == "__main__":
 	luigi.run(main_task_cls=RunManyLetters)
